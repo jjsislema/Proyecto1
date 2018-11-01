77a13603b1004562c17f88ad94af01ed73901ca2
rigojr <rigojunnior@gmail.com>
2018-11-01 00:28:13 -0400

    Configurando el Instant State de prueba

--- app/src/main/java/com/example/jefferson/proyecto1/MainActivity.java
+++ app/src/main/java/com/example/jefferson/proyecto1/MainActivity.java
@@ -4,17 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String textoEjemplo = "HOLA SOY SOLO UN EJEMPLO";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            TextView textView = (TextView)findViewById(R.id.textView0);
+            textView.setText(savedInstanceState.getString("txtTextView0"));
+        }
     }
 
     public void showList(View view) {
         Intent intent = new Intent(this,ListaArticulos.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView textView = (TextView)findViewById(R.id.textView0);
+        outState.putString( "txtTextView0" , textView.getText().toString() );
+    }
+
+    public void onClickTest(View view){
+        TextView textView = (TextView)findViewById(R.id.textView0);
+        textView.setText(textoEjemplo);
+    }
+
 }
