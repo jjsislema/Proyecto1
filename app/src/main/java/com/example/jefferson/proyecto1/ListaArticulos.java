8f4e667681eaad7de0b7a1bbca32ee864732f69f
fabian graterol <fabiancgraterol@gmail.com>
2018-11-01 17:55:07 -0400

    Agregacion de navegacion ancestral y scrollview

--- app/src/main/java/com/example/jefferson/proyecto1/ListaArticulos.java
+++ app/src/main/java/com/example/jefferson/proyecto1/ListaArticulos.java
@@ -17,8 +17,8 @@ public class ListaArticulos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_articulos);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
